0d07cf5f100fe1e798b571ebc0aee7aae761fc84
eyoelm5 <eyoelmoges20@gmail.com>
2024-08-09 16:02:11 +0300

    Fix issues

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   base: "Social-Blogging-App",
   server:{
-    port:3000,
     proxy:{
       '/api': {
         target: 'https://mern-w0x6.onrender.com',
