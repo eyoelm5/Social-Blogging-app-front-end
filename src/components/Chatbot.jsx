424744d270e14bf64e0b5405e58d2c407268e47b
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-11 02:55:26 +0300

    expand chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import chatbot from "../assets/chatbot.png";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 const Chatbot = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div>
-      <Link target="_blank" to="https://zeresenayyaregalchatbot.streamlit.app/">
-        <img
-          src={chatbot}
-          alt="Fixed Image"
-          className="fixed bottom-1 right-1 w-16 h-16 z-10"
-        />
-      </Link>
+    <div className="fixed bottom-1 right-1 z-10">
+      <button
+        onClick={toggleExpansion}
+        className="bg-blue-500 text-white p-2 rounded-full shadow-md focus:outline-none"
+      >
+        {isExpanded ? "Collapse" : "Expand"}
+      </button>
+
+      {isExpanded && (
+        <div className="mt-2 bg-white rounded-lg shadow-lg">
+          <iframe
+            src="https://www.chatbase.co/chatbot-iframe/skvxT41WhTZr3YQMULUdr"
+            width="100%"
+            style={{ height: "100%", minHeight: "700px" }}
+            frameBorder="0"
+            title="Chatbot"
+            className="rounded-b-lg"
+          ></iframe>
+        </div>
+      )}
     </div>
   );
 };
