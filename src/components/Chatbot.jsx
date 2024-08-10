7a86d3b1f0fc3cd80f479835c2bda152698b7a3f
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-11 02:35:37 +0300

    chatbot reversed

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -5,12 +5,13 @@ import { Link } from "react-router-dom";
 const Chatbot = () => {
   return (
     <div>
-      <iframe
-        src="https://www.chatbase.co/chatbot-iframe/skvxT41WhTZr3YQMULUdr"
-        width="100%"
-        style="height: 100%; min-height: 700px"
-        frameborder="0"
-      ></iframe>
+      <Link target="_blank" to="https://zeresenayyaregalchatbot.streamlit.app/">
+        <img
+          src={chatbot}
+          alt="Fixed Image"
+          className="fixed bottom-1 right-1 w-16 h-16 z-10"
+        />
+      </Link>
     </div>
   );
 };
