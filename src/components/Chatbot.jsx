7a487b1968e86979c2a855cdf83deff5494b323b
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-11 02:32:10 +0300

    chatbot updated

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -5,13 +5,12 @@ import { Link } from "react-router-dom";
 const Chatbot = () => {
   return (
     <div>
-      <Link target="_blank" to="https://zeresenayyaregalchatbot.streamlit.app/">
-        <img
-          src={chatbot}
-          alt="Fixed Image"
-          className="fixed bottom-1 right-1 w-16 h-16 z-10"
-        />
-      </Link>
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/skvxT41WhTZr3YQMULUdr"
+        width="100%"
+        style="height: 100%; min-height: 700px"
+        frameborder="0"
+      ></iframe>
     </div>
   );
 };
