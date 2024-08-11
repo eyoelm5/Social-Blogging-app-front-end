7455a6f4fa7014937d7933ce04a04b1005ece718
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-11 03:02:36 +0300

    Using react icons

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaRobot, FaTimes } from "react-icons/fa"; 
 
 const Chatbot = () => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -11,9 +12,9 @@ const Chatbot = () => {
     <div className="fixed bottom-1 right-1 z-10">
       <button
         onClick={toggleExpansion}
-        className="bg-blue-500 text-white p-2 rounded-full shadow-md focus:outline-none"
+        className="bg-blue-500 text-white p-3 rounded-full shadow-md focus:outline-none"
       >
-        {isExpanded ? "Collapse" : "Expand"}
+        {isExpanded ? <FaTimes size={20} /> : <FaRobot size={20} />}
       </button>
 
       {isExpanded && (
@@ -21,7 +22,7 @@ const Chatbot = () => {
           <iframe
             src="https://www.chatbase.co/chatbot-iframe/skvxT41WhTZr3YQMULUdr"
             width="100%"
-            style={{ height: "100%", minHeight: "700px" }}
+            style={{ height: "70%", minHeight: "700px" }}
             frameBorder="0"
             title="Chatbot"
             className="rounded-b-lg"
