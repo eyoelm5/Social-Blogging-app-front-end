a43e3d1e853ab1d4f9a005e6383da0a9f6de0b5b
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-12 21:59:10 +0300

    footer height added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-black w-full">
+    <div className="bg-black w-full min-h-[100px]">
       <div className="copyright text-white italic font-thin text-center pb-4 pt-1">
         <p>All Rights Reserved &copy; Directed Foundation</p>
       </div>
