463deaa41d8e8cb06f0c88632f804581e13052e9
eyoelm5 <eyoelmoges20@gmail.com>
2024-08-13 21:32:14 +0300

    Edit some styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-black w-full min-h-[150px] flex items-end justify-center p-4">
+    <div className="bg-black w-full flex items-end justify-center p-4">
       <div className="copyright text-white italic font-thin text-center">
         <p>All Rights Reserved &copy; Directed Foundation</p>
       </div>
